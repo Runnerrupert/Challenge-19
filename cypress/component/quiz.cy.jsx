9821ced9a4758e2b5c86ae21dcea2c5401fa3f66
Cameron Barfuss <Cameron.barfuss@gmail.com>
2024-10-24 16:14:07 -0600

    add comments to component and e2e testing files

--- cypress/component/quiz.cy.jsx
+++ cypress/component/quiz.cy.jsx
@@ -1,7 +1,8 @@
 import Quiz from '../../client/src/components/Quiz';
-
+// Testing Main Quiz Component
 describe('Quiz Component', () => {
     beforeEach(() => {
+        // Make an API request before each test
         cy.intercept({
             method: 'GET',
             url: '/api/questions/random'
@@ -13,13 +14,15 @@ describe('Quiz Component', () => {
         ).as('getRandomQuestion');
     })
 
+    // Checks if the quiz starts and shows the first question
     it('should start the quiz and make the first question appear', () => {
         cy.mount(<Quiz/>);
         cy.get('button').contains('Start Quiz').click();
         cy.get('.card').should('be.visible');
         cy.get('h2').should('be.visible');
     });
-
+    
+    // Checks if the next question shows after the first is answered
     it ('should answer the first question', () => {
         cy.mount(<Quiz/>);
         cy.get('button').contains('Start Quiz').click();
@@ -27,6 +30,7 @@ describe('Quiz Component', () => {
         cy.get('h2').contains('Which of the following is a mutable data type in Python?');
     });
 
+    // Answers each of the 3 questions
     it ('should complete the quiz from start to finish', () => {
         cy.mount(<Quiz/>);
         cy.get('button').contains('Start Quiz').click();
@@ -35,6 +39,7 @@ describe('Quiz Component', () => {
         cy.get('button').contains('3').click();
     });
     
+    // Checks if the quiz restarts after finishing, makes sure the completion screen is visible along with the first question card
     it ('should restart the quiz after completion', () => {
         cy.mount(<Quiz/>);
         cy.get('button').contains('Start Quiz').click();
--- cypress/e2e/quiz.cy.ts
+++ cypress/e2e/quiz.cy.ts
@@ -1,19 +1,23 @@
 describe('Quiz Component', () => {
+    // Visits the root route before each test is ran
     beforeEach(() => {
         cy.visit('/');
     });
 
+    // Checks if the quiz starts and shows the first question
     it('should start the quiz and make the first question appear', () => {
         cy.get('button').contains('Start Quiz').click();
         cy.get('.card').should('be.visible');
         cy.get('h2').should('be.visible');
     });
 
+    // Checks if the next question shows after the first is answered
     it ('should answer the first question', () => {
         cy.get('button').contains('Start Quiz').click();
         cy.get('button').contains('2').click();
     });
 
+    // Answers all 10 questions using a for loop
     it ('should complete the quiz from start to finish', () => {
         cy.get('button').contains('Start Quiz').click();
         for(let i = 0; i < 10; i++) {
@@ -23,6 +27,7 @@ describe('Quiz Component', () => {
         cy.get('.alert-success').should('be.visible').and('contain','Your score:');
     });
 
+    // Checks if the quiz restarts after finishing, makes sure the completion screen is visible
     it ('should restart the quiz after completion', () => {
         cy.get('button').contains('Start Quiz').click();
         for(let i = 0; i < 10; i++) {
